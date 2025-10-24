67c161233d1eaaabc260f7263e89886b690f9988
Rafael Aquino <rafa.hdez55@gmail.com>
2025-10-24 15:56:26 -0600

    Avance tarea - registro y login

--- aquinohernandez_20251016/routes/api.php
+++ aquinohernandez_20251016/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ZonaController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,3 +16,7 @@ Route::get('/zona/{idzona}', [zonaController::class, 'obtenerZona']);
 Route::get('/zonaspais/{idpais}', [zonaController::class, 'obtenerZonaPais']);
 
 Route::post('/nuevazona', [zonaController::class, 'crearZona']);
+
+Route::post('/nuevousuario', AuthController::class, 'nuevoUsuario');
+
+Route::post('/login', [AuthController::class, 'login']);
